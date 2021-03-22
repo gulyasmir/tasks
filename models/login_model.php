089020742c84606d0541910991aa5c6ad7008e1b
Gulnara <gulyasmir@yandex.ru>
2021-03-22 16:32:17 +0300

    обработка ошибки авторизации

--- models/login_model.php
+++ models/login_model.php
@@ -19,6 +19,16 @@ class Login_Model extends Model {
             Session::init();
             Session::set('loggedIn', true);
             header('Location: '.URL.'/index');
+        } else {
+            echo '<div
+                        style="padding-top: 10%;
+                        color:#ff0000;
+                        text-align: center;
+                        font-size:120%;"
+                        >
+                        Логин или  пароль неверен! <br><br>
+                         <a class="nav-link" aria-current="page" href="'. URL.'"index">
+                        На главную</a></div>';
         }
 
     }
