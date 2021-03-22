d38d502af612871c5603ed8be47ee7b10f2d2ea6
Gulnara <gulyasmir@yandex.ru>
2021-03-22 16:53:47 +0300

    меню для мобильной версии

--- views/header.php
+++ views/header.php
@@ -11,7 +11,11 @@
 <?php Session::init(); ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <div id="header">
+            <a class="navbar-brand" href="#">Задачник</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#header" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="header" class="collapse navbar-collapse">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="<?php echo URL; ?>index">Список задач</a>
