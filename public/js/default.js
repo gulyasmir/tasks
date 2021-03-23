cb1c4b4aa6e0b782114980c9e7b83c4d761b73a0
Гульнара  Смирнова <gulyasmir@yandex.ru>
2021-03-23 14:28:12 +0300

    Update default.js

--- public/js/default.js
+++ public/js/default.js
@@ -9,14 +9,10 @@ $(document).ready(function () {
         ));
         return matches ? decodeURIComponent(matches[1]) : undefined;
     }
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+
     (function () {
         'use strict'
-
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.querySelectorAll('.needs-validation')
-
-        // Loop over them and prevent submission
         Array.prototype.slice.call(forms)
             .forEach(function (form) {
                 form.addEventListener('submit', function (event) {
