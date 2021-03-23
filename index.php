56d13789430ca19ac927c8fca89740e968430e82
Gulnara <gulyasmir@yandex.ru>
2021-03-23 10:39:09 +0300

    автозагрузка

--- index.php
+++ index.php
@@ -6,14 +6,12 @@ function autoLoader() {
     $count_libs = count($libs);
     for ($i=0;$i<$count_libs;$i++){
         require 'libs/'.$libs[$i].'.php';
-        echo 'libs/'.$libs[$i].'.php';
     }
 
     $config = ['paths','database','config'];
     $count_config = count($config);
     for ($i=0;$i<$count_config;$i++){
         require 'libs/'.$config[$i].'.php';
-        echo 'libs/'.$config[$i].'.php';
     }
 }
 
